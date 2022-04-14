4a02dfad5d21268fc27ef451b6e3f85b78ce7d24
Ahlytlex <66805063+Ahlytlex@users.noreply.github.com>
2022-04-14 12:55:39 -0400

    Inital Commit

---
+++ wikiServer/wikiApp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+# create all url patterns - this one routes .../mysite/appname/index to the index function in your views file.
+urlpatterns = [
+
+    path("index", views.index)
+
+]
